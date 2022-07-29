611cdbc264c3728ddaafb1f77d77f0e5e0737fef
Ryan J. Parker <ryan.j.parker@wsu.edu>
2022-07-29 09:28:29 -0700

    fix function to display array

--- render-sports.js
+++ render-sports.js
@@ -1,11 +1,6 @@
-export function renderSportsItem(sport) {
-    const pTag = document.createElement('p';)
-    pTag.textContent = sport;
-    pTag.classList.add('aSport');
-    return pTag;
-function renderSports(sportsGame) {
+export function renderSports(sport) {
     const pTag = document.createElement('p');
-    pTag.textContent = sportsGame;
+    pTag.textContent = sport;
     pTag.classList.add('aSport');
     return pTag;
 }
